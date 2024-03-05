d612aa16d27c34555acd55d9b90f5b100e79fe6e
MarilynLSerna <160423192+MarilynLSerna@users.noreply.github.com>
2024-03-04 16:48:45 -0800

    Updated

---
+++ Basic_HTML_andJavaScript_2/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <html>
+        <script>alert("Alert Box")</script>;
+        <script>
+            function My_First_Function()    {
+            var String = "Kiss me, I'm Irish!";
+            document.getElementById("Irish").innerHTML =
+            result:
+        }
+        </script>
+    </htmly>
